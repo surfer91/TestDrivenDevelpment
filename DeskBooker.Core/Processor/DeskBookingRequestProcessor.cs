24cf69078d623606db0a40a18121a12bcde3c046
Krzysztof Klimczak <l2eu@wp.pl>
2020-09-24 20:59:18 +0200

    ImplementStoreTheIDDesk

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -23,8 +23,10 @@ namespace DeskBooker.Core.Processor
             }
 
             var availableDesks=_deskRepository.GetAvailableDesks(request.Date);
-                if (availableDesks.Count()>0){
-            _deskBookingRepository.Save(Create<DeskBooking>(request));
+                if (availableDesks.FirstOrDefault() is Desk availableDesk){
+                    var deskBooking=Create<DeskBooking>(request);
+                    deskBooking.DeskId=availableDesk.Id;
+            _deskBookingRepository.Save(deskBooking);
             }
 
             return Create<DeskBookingResult>(request);}
