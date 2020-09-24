219fd32da3edc54734e84c052655a7d658f6d93f
Krzysztof Klimczak <l2eu@wp.pl>
2020-09-24 21:28:11 +0200

    ImplementReturn

--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -21,15 +21,18 @@ namespace DeskBooker.Core.Processor
             {
                 throw new ArgumentNullException(nameof(request));
             }
-
+                var result=Create<DeskBookingResult>(request);
             var availableDesks=_deskRepository.GetAvailableDesks(request.Date);
                 if (availableDesks.FirstOrDefault() is Desk availableDesk){
                     var deskBooking=Create<DeskBooking>(request);
                     deskBooking.DeskId=availableDesk.Id;
             _deskBookingRepository.Save(deskBooking);
+            result.Code=DeskBookingResultCode.Success;
             }
-
-            return Create<DeskBookingResult>(request);}
+            else
+            {result.Code=DeskBookingResultCode.NoDeskAvailable;
+            }
+            return result;}
 
          private static T Create<T>(DeskBookingRequest request) where T :DeskBookingBase,new()
         {
