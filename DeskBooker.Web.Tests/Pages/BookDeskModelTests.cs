2a7c266196408e63cabd2a9042379e2b6963d221
Krzysztof Klimczak <l2eu@wp.pl>
2020-09-25 20:53:02 +0200

    TestCheckIftheModelisValid

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -7,15 +7,21 @@ using Moq;
 namespace DeskBooker.Web.Pages
 {
   public class BookDeskModelTests
-  {[Fact]
-  public void ShouldCallBookDeskMethodOfProcessor(){
+  {[Theory]
+  [InlineData(1,true)]
+  [Inline(0,false)]
+  public void ShouldCallBookDeskMethodOfProcessorIfModelIsValid(int expectedBookDeskCalls,bool isModelValid){
 
     var processorMock=new Mock<IDeskBookingRequestProcessor>();
     var bookDeskModel=new BookDeskModel(processorMock.Object){
       DeskBookingRequest=new DeskBookingRequest()
     };
+
+    if (!isModelValid){
+      bookDeskModel.ModelState.AddModelError("JustAKey","Error massage");
+    }
     bookDeskModel.OnPost();
-    processorMock.Verify(x =>x.BookDesk(bookDeskModel.DeskBookingRequest),Times.Once);
+    processorMock.Verify(x =>x.BookDesk(bookDeskModel.DeskBookingRequest),Times.Exactly(expectedBookDeskCalls));
   }
 
   }
