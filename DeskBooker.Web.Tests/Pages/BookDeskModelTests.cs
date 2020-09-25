4b0e4eccb6127d1d9b6d20b147ec9f61aec7e00d
Krzysztof Klimczak <l2eu@wp.pl>
2020-09-25 21:35:11 +0200

    TestAddModelError

--- DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
+++ DeskBooker.Web.Tests/Pages/BookDeskModelTests.cs
@@ -25,5 +25,24 @@ namespace DeskBooker.Web.Pages
     processorMock.Verify(x =>x.BookDesk(bookDeskModel.DeskBookingRequest),Times.Exactly(expectedBookDeskCalls));
   }
 
+  [Fact]
+
+  public void ShouldAddModelErrorIfNoDeskIsAvailable(){
+        var processorMock=new Mock<IDeskBookingRequestProcessor>();
+    var bookDeskModel=new BookDeskModel(processorMock.Object){
+      DeskBookingRequest=new DeskBookingRequest()
+    };
+processorMock.Setup(x =>x.BookDesk(bookDeskModel.DeskBookingRequest)).Returns(new DeskBookingResult
+{
+Code=DeskBookingResultCode.NoDeskAvailable
+});
+
+bookDeskModel.OnPost();
+var modelStateEntry=
+Assert.Contains("DeskBookingRequest.Date",bookDeskModel.ModelState);
+var modelError=Assert.Single(modelStateEntry.Errors);
+Assert.Equal("No desk available for selected date", modelError.ErrorMessage);
+  }
+
   }
 }
